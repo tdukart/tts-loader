74e371608b5fa9ce51723ad77dd630fdb26983e6
Todd Dukart <tdukart@aloftinteractive.com>
2017-05-24 21:39:32 -0400

    Convert output to MP3

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 var execSync = require( 'child_process' ).execSync,
+	ffmpeg = require( 'fluent-ffmpeg' ),
 	loaderUtils = require( 'loader-utils' ),
 	tmp = require( 'tmp' );
 
 module.exports = function ( source ) {
-	var tempFile = tmp.tmpNameSync( {
+	var callback = this.async();
+	var _this = this;
+
+	var tmpAiff = tmp.fileSync( {
 		postfix: '.tmp.aiff'
 	} );
 
-	var command = 'say -o ' + tempFile,
+	var command = 'say -o ' + tmpAiff.name,
 		options = { input: source };
 
 	execSync( command, options );
 
-	var fileContent = this.fs.readFileSync( tempFile );
+	var tmpMp3 = tmp.fileSync( {
+		postfix: '.tmp.mp3'
+	} );
 
-	var fileName = loaderUtils.interpolateName(
-		this, '[name].[hash].aiff', { content: fileContent }
-	);
+	ffmpeg()
+		.input( tmpAiff.name )
+		.audioCodec( 'libmp3lame' )
+		.save( tmpMp3.name )
+		.on( 'end', function () {
+			var outputStream = _this.fs.readFileSync( tmpMp3.name );
 
-	this.emitFile( fileName, fileContent );
+			var fileName = loaderUtils.interpolateName(
+				_this, '[name].[hash].mp3', { content: outputStream }
+			);
 
-	var path = '__webpack_public_path__ + ' + JSON.stringify( fileName );
+			_this.emitFile( fileName, outputStream );
 
-	return 'export default ' + path;
+			var path = '__webpack_public_path__ + ' + JSON.stringify( fileName );
+
+			callback( null, 'export default ' + path );
+		} );
 };
